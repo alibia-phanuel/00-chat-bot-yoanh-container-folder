02a7740d4dee4cd50c718b30e5134a0277d51538
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2025-02-25 06:21:00 +0100

    Fix Node.js version for Heroku

--- server-hybride/@types/express.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from "express";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    userId?: number; // 🔥 Ajoute `userId` au type `Request`
-  }
-}
--- server-hybride/src/routes/homeRoutes.routes.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getHomePage } from "../controllers/homeController";
-const router = express.Router();
-// Route pour afficher la page d'accueil
-router.get("/", getHomePage);
-export default router;
